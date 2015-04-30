e6ef37901d1c0dc9f54e85549b57d45fea48a9ed
Filomeno Santos <filomenosantos@gmail.com>
2015-04-30 19:23:57 +0200

    Create new cities

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencode = bodyParser.urlencoded({extended: false});
+
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
@@ -11,9 +14,20 @@ app.get('/', function(req, res) {
 
 module.exports = app;
 
+var cities = {
+	'Lisboa': 'some desc', 
+	'Barcelona': 'des', 
+	'Madrid': 'descr'
+};
+
 app.get('/cities', function(request, response) {
-	var cities = ['Lisboa', 'Barcelona', 'Madrid'];
-	response.json(cities);
+	response.json(Object.keys(cities));
+});
+
+app.post('/cities', urlencode, function(request, response) {
+	var newCity = request.body;
+	cities[newCity.name] = newCity.description; 
+	response.status(201).json(newCity.name);
 });
 
 
