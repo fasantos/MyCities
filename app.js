5fed8668c17829002f13d6178635c63d290652ef
Filomeno Santos <filomenosantos@gmail.com>
2015-04-30 17:13:07 +0200

    Listing cities on index.html

--- app.js
+++ app.js
@@ -3,11 +3,18 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+
 app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
 module.exports = app;
+
+app.get('/cities', function(request, response) {
+	var cities = ['Lisboa', 'Barcelona', 'Madrid'];
+	response.json(cities);
+});
 
 
 
